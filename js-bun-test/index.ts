00f3386ebd982ef870fd6056592afb89d2b66aa1
TeoOrt <mateo041ortega@gmail.com>
2025-02-17 21:07:05 -0700

    Got parsing and inserting to db working on javascript

--- js-bun-test/index.ts
+++ js-bun-test/index.ts
@@ -5,32 +5,23 @@
 
 import { resolve } from "bun";
 import Database from "bun:sqlite";
+import { Book } from "./types/types";
+import { book_insert, create_table } from "./libs/database";
 
-type Author = {
-  key: string | null;
-  name: string | null;
-};
-
-type Book = {
-  key: string;
-  title: string;
-  cover_id: number;
-  subject: string[];
-  authors: Author[];
-};
-//
 const WS_URL = "ws://localhost:3000";
 const client = new WebSocket(WS_URL);
-const db = new Database("../Test.db");
-const query = db.query("select 'Hello world' as message;");
-const res = query.get();
+const db: Database = new Database("../Test.db", { strict: true });
+create_table(db);
 
 console.log("Started listening at local host 3000");
 client.addEventListener("open", (event) => {
   client.send("Hey");
 });
 
+var i = false;
 client.addEventListener("message", (event) => {
   const data: string = event.data.toString();
-  const book: Book[] = JSON.parse(data);
+  const parsed = JSON.parse(data);
+  const book: Book = parsed;
+  book_insert(db, book);
 });
